faf98d45cd8b176537181f403d9779eb2c765b1d
Daniel Knezevic <danielknezevic90@gmail.com>
2026-05-30 16:57:04 +0200

    MainScreen: Set voltage/current labels based on supply type

--- firmware/src/gui/main_screen.cpp
+++ firmware/src/gui/main_screen.cpp
@@ -1,5 +1,6 @@
 #include "main_screen.h"
 
+#include "config.h"
 #include "pdsink_iface.h"
 #include "tiny_format.h"
 
@@ -20,10 +21,12 @@ auto MainScreen::build() -> uint8_t* {
     printString(m_width / 2, 0, tinyFormat("%05.2fV", m_measured_voltage),
                 {.align = TextAlign::center, .size = FontSize::big});
 
-    // Target voltage in mV
+    // Target/Limit voltage in mV
+    std::string voltage_label =
+        (m_supply_mode == SupplyMode::CV) ? "TARGET" : "LIMIT";
     auto target_voltage_pos =
-        (m_width - printString(0, 0, "TARGET 00000mV", true)) / 2;
-    auto len = printString(target_voltage_pos, 16, "TARGET ");
+        (m_width - printString(0, 0, voltage_label + " 00000mV", true)) / 2;
+    auto len = printString(target_voltage_pos, 16, voltage_label + " ");
     len += printString(target_voltage_pos + len, 16,
                        tinyFormat("%05d", m_target_voltage),
                        {.invert = m_is_target_voltage_selected});
@@ -33,12 +36,14 @@ auto MainScreen::build() -> uint8_t* {
     printString(m_width / 2, 25, tinyFormat("%05.2fA", m_measured_current),
                 {.align = TextAlign::center, .size = FontSize::big});
 
-    // Target current in mA
+    // Target/Limit current in mA
+    std::string current_label =
+        (m_supply_mode == SupplyMode::CV) ? "LIMIT" : "TARGET";
     auto target_current_pos =
-        (m_width - printString(0, 0, "LIMIT 0000mA", true)) / 2;
-    len = printString(target_current_pos, 41, "LIMIT ");
+        (m_width - printString(0, 0, current_label + " 0000mA", true)) / 2;
+    len = printString(target_current_pos, 41, current_label + " ");
     len += printString(target_current_pos + len, 41,
-                       tinyFormat("%04dV", m_target_current),
+                       tinyFormat("%04d", m_target_current),
                        {.invert = m_is_target_current_selected});
     printString(target_current_pos + len, 41, "mA");
 
