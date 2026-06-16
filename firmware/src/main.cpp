72cf11802c8b845aed5943b6ce08903d71635eec
Daniel Knezevic <danielknezevic90@gmail.com>
2026-06-16 19:56:06 +0200

    Main: Initialize hardware in a separate function

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -53,23 +53,22 @@ static constexpr PicoGpioPin g_vout_status{k_g_vout_status_pin};
 static constexpr PicoGpioPin g_pd_int{k_g_pd_int_pin};
 static constexpr PicoI2c g_i2c{k_i2c};
 PicoRepeatingTimer g_timer;
+RotaryEncoder g_rotary_encoder{g_rot_enc_a_pin, g_rot_enc_b_pin,
+                               g_rot_enc_btn_pin};
+Ssd1306_128x64 g_oled{g_i2c};
+Ina226 g_ina226{g_i2c, k_ina226_addr};
+Ap33772 g_ap33772{g_i2c};
+Ap33772s g_ap33772s{g_i2c};
+std::reference_wrapper<IPdSink> g_pdsink = g_ap33772;
 
 volatile uint32_t g_system_time = 0;
 volatile bool g_is_g_pd_interrupt_pending = false;
 volatile bool g_is_g_vout_status_interrupt_pending = false;
 static std::array<uint8_t, Ssd1306_128x64::getFrameBufferSize()> g_frame_buffer;
 
-auto main() -> int {
-
-    RotaryEncoder rotary_encoder{g_rot_enc_a_pin, g_rot_enc_b_pin,
-                                 g_rot_enc_btn_pin};
-    Ssd1306_128x64 oled{g_i2c};
-    Ina226 ina226{g_i2c, k_ina226_addr};
-    Ap33772 ap33772{g_i2c};
-    Ap33772s ap33772s{g_i2c};
-
+auto initialize() -> void {
     g_i2c.initialize(k_i2c_sda_pin, k_i2c_scl_pin, k_i2c_speed);
-    rotary_encoder.initialize();
+    g_rotary_encoder.initialize();
     g_output_enable.configure(Direction::Output, Pull::Down);
     g_vout_status.configure(Direction::Input, Pull::Down);
     g_vout_status.attachInterrupt(
@@ -86,41 +85,45 @@ auto main() -> int {
         },
         nullptr);
     g_pd_int.enableInterrupt(true);
-    oled.initialize();
-    ina226.setAveragingMode(Ina226::AveragingMode::Samples128);
-    ina226.calibrate(5, 0.01);
+    g_oled.initialize();
+    g_ina226.setAveragingMode(Ina226::AveragingMode::Samples128);
+    g_ina226.calibrate(5, 0.01);
     Screen::initialize(g_frame_buffer, Ssd1306_128x64::getWidth(),
                        Ssd1306_128x64::getHeight(),
                        Ssd1306_128x64::getPageHeight());
     // By default, use AP33772 if available, otherwise fall back to AP33772S
-    std::reference_wrapper<IPdSink> pdsink = ap33772;
-    if (ap33772.probe()) {
-        ap33772.setNtc(k_ntc_tr25, k_ntc_tr50, k_ntc_tr75, k_ntc_tr100);
-        ap33772.setOtpThreshold(k_otp_threshold);
+    if (g_ap33772.probe()) {
+        g_ap33772.setNtc(k_ntc_tr25, k_ntc_tr50, k_ntc_tr75, k_ntc_tr100);
+        g_ap33772.setOtpThreshold(k_otp_threshold);
         Ap33772::MaskReg mask;
         mask.newpdo_en = 1;
         mask.ocp_en = 1;
         mask.otp_en = 1;
         mask.ovp_en = 1;
-        ap33772.setMask(mask);
+        g_ap33772.setMask(mask);
     } else {
-        pdsink = std::ref(ap33772s);
-        ap33772s.setNtc(k_ntc_tr25, k_ntc_tr50, k_ntc_tr75, k_ntc_tr100);
-        ap33772s.setOtpThreshold(k_otp_threshold);
-        ap33772s.setVselMin(k_ap33772s_vsel_min);
+        g_pdsink = std::ref(g_ap33772s);
+        g_ap33772s.setNtc(k_ntc_tr25, k_ntc_tr50, k_ntc_tr75, k_ntc_tr100);
+        g_ap33772s.setOtpThreshold(k_otp_threshold);
+        g_ap33772s.setVselMin(k_ap33772s_vsel_min);
         Ap33772s::MaskReg mask;
         mask.newpdo_msk = 1;
         mask.ocp_msk = 1;
         mask.otp_msk = 1;
         mask.ovp_msk = 1;
         mask.uvp_msk = 1;
-        ap33772s.setMask(mask);
+        g_ap33772s.setMask(mask);
     }
     g_timer.start(
         1, [](void* ctx) -> void { g_system_time = g_system_time + 1; },
         nullptr);
-    HardwareContext hardware{
-        .pdsink = pdsink.get(), .output_enable = g_output_enable, .oled = oled};
+}
+
+auto main() -> int {
+    initialize();
+    HardwareContext hardware{.pdsink = g_pdsink.get(),
+                             .output_enable = g_output_enable,
+                             .oled = g_oled};
 
     StateMachine state_machine{hardware};
 
@@ -132,25 +135,25 @@ auto main() -> int {
         uint32_t delta = current_time - last_tick_time;
         last_tick_time = current_time;
 
-        rotary_encoder.handle(current_time);
-        auto encoder_state = rotary_encoder.getState();
+        g_rotary_encoder.handle(current_time);
+        auto encoder_state = g_rotary_encoder.getState();
         if (encoder_state != RotaryEncoder::State::idle &&
             encoder_state != RotaryEncoder::State::processed) {
             state_machine.dispatch(RotaryEncoderEvent{encoder_state});
-            rotary_encoder.clearState();
+            g_rotary_encoder.clearState();
         }
 
         if (current_time - last_sensor_read_time >= k_sensor_read_period) {
             last_sensor_read_time = current_time;
-            state_machine.dispatch(SensorUpdateEvent{ina226.getBusVoltage(),
-                                                     ina226.getCurrent(),
-                                                     pdsink.get().getTemp()});
+            state_machine.dispatch(SensorUpdateEvent{g_ina226.getBusVoltage(),
+                                                     g_ina226.getCurrent(),
+                                                     g_pdsink.get().getTemp()});
         }
 
         if (g_is_g_pd_interrupt_pending) {
             g_is_g_pd_interrupt_pending = false;
             state_machine.dispatch(
-                PdSinkStatusUpdateEvent{pdsink.get().getStatus()});
+                PdSinkStatusUpdateEvent{g_pdsink.get().getStatus()});
         }
 
         if (g_is_g_vout_status_interrupt_pending) {
